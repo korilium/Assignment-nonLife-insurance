9e2d17c2781e629bb6ea66824bdbda1506b8ccaf
korilium <ignace.decocq@hotmail.com>
2021-04-24 17:33:09 +0200

    data exploration and working on glm gam

---
+++ Severity_Analysis/exploratoryanalysis.r
@@ -0,0 +1,75 @@
+
+library(gridExtra)
+data <- read.csv2(file= "Severity_Analysis/train.csv", sep= ",")
+
+# factorize 
+Data <- as.data.frame(data)
+train<- Data %>% select(-X) %>% 
+ mutate(across(c(claimAm), as.numeric)) %>%
+ mutate(across(c(agecar, sexph, power, split, fuel, use, fleet, sportc, cover, ageph), as.factor))
+
+str(train)
+dim(train)
+
+######### exploratory data analysis ##############
+#plot density claimAm 
+ggplot(train, aes(x =claimAm)) +
+geom_density() +
+scale_x_continuous(trans = 'log2') +
+geom_histogram(aes(y = ..density..), fill = "#116e8a", color = "white", alpha = 0.7) +
+geom_rug()
+
+plot <- list()
+
+for(i in colnames(train)){
+  plot[[i]] <-   ggplot(data = train, mapping = aes_string(x = i, y = "claimAm")) +
+  geom_jitter(aes( color = 'red'), alpha = 0.2) +
+  geom_boxplot(,fill = "#116e8a", color = "black", alpha = 0.3) +
+    labs(x = i) +
+  guides(color = FALSE) +
+  theme_minimal()
+}
+
+grid.arrange(plot[[1]], plot[[9]], plot[[4]], 
+            plot[[5]], plot[[6]], plot[[7]], 
+            plot[[8]], plot[[10]], plot[[11]], plot[[12]], ncol= 3)
+
+
+#map of balgium 
+post_expo <- train %>% group_by(postal) %>% summarize(num = n(), tot_freq = sum(freq))
+post_expo %>% slice(1:5)
+
+belgium_shape_sf <- left_join(belgium_shape_sf, post_expo, by = c("POSTCODE" = "postal"))
+
+belgium_shape_sf$claimAm <- belgium_shape_sf$tot_freq / belgium_shape_sf$Shape_Area
+
+belgium_shape_sf$claimAm_class <- cut(belgium_shape_sf$claimAm,
+                                   breaks = quantile(belgium_shape_sf$claimAm,
+                                   c(0, 0.2, 0.8, 1), na.rm = TRUE),
+                                   right = FALSE, include.lowest = TRUE,
+                                   labels = c("low", "average", "high"))
+
+ggplot(belgium_shape_sf) +
+geom_sf(aes(fill = claimAm_class), colour = "black", size = 0.1) +
+ggtitle("MTPLclaimamountcydata") +
+labs(fill = "Relative\nclaimamount") +
+scale_fill_brewer(palette = "Blues", na.value = "white") +
+theme_bw()
+
+
+train %>% plot_bar()
+train %>% plot_histogram()
+
+
+Data_group <- splitmix(train)
+Data_quanti <- Data_nozero[Data_group$col.quant]
+Data_quali <- Data_nozero[Data_group$col.qual]
+
+tree <- hclustvar(X.quanti = Data_quanti, X.quali = Data_quali)
+plot(tree)
+
+# mean, variance skewnnes and kurtosis
+sapply(Data_quanti, mean)
+sapply(Data_quanti, var)
+sapply(Data_quanti, skewness)
+sapply(Data_quanti, kurtosis)
