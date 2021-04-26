4d7c2d74823ab39135791d3a59519252fd5ca884
korilium <ignace.decocq@hotmail.com>
2021-04-26 16:09:12 +0200

    after meeting 26/4

--- Severity_Analysis/exploratoryanalysis.r
+++ Severity_Analysis/exploratoryanalysis.r
@@ -1,19 +1,30 @@
 
 library(gridExtra)
-data <- read.csv2(file= "Severity_Analysis/train.csv", sep= ",")
+library(ggplot2)
+library(dplyr)
+library(ClustOfVar)
+library(PCAmixdata)
+library(visdat)
+library(httpgd)
+library(skimr)
+library(DataExplorer)
+hgd()
+hgd_browse()
+
+data <- read.csv2(file = "Severity_Analysis/train.csv", sep = ",")
 
 # factorize 
 Data <- as.data.frame(data)
-train<- Data %>% select(-X) %>% 
- mutate(across(c(claimAm), as.numeric)) %>%
- mutate(across(c(agecar, sexph, power, split, fuel, use, fleet, sportc, cover, ageph), as.factor))
+train <- Data %>% select(-X) %>%
+ mutate(across(c(claimAm, lat, long, logclaimAm), as.numeric)) %>%
+ mutate(across(c(agecar, sexph, power, split, fuel, use, fleet, sportc, cover, ageph, group_ageph), as.factor))
 
 str(train)
 dim(train)
 
 ######### exploratory data analysis ##############
 #plot density claimAm 
-ggplot(train, aes(x =claimAm)) +
+ggplot(train, aes(x = claimAm)) +
 geom_density() +
 scale_x_continuous(trans = 'log2') +
 geom_histogram(aes(y = ..density..), fill = "#116e8a", color = "white", alpha = 0.7) +
@@ -21,22 +32,22 @@ geom_rug()
 
 plot <- list()
 
-for(i in colnames(train)){
-  plot[[i]] <-   ggplot(data = train, mapping = aes_string(x = i, y = "claimAm")) +
-  geom_jitter(aes( color = 'red'), alpha = 0.2) +
-  geom_boxplot(,fill = "#116e8a", color = "black", alpha = 0.3) +
+for (i in colnames(train)) {
+  plot[[i]] <- ggplot(data = train, mapping = aes_string(x = i, y = "logclaimAm")) +
+  geom_jitter(aes(color = 'red'), alpha = 0.2) +
+  geom_boxplot(, fill = "#116e8a", color = "black", alpha = 0.3) +
     labs(x = i) +
   guides(color = FALSE) +
   theme_minimal()
 }
 
-grid.arrange(plot[[1]], plot[[9]], plot[[4]], 
-            plot[[5]], plot[[6]], plot[[7]], 
-            plot[[8]], plot[[10]], plot[[11]], plot[[12]], ncol= 3)
+grid.arrange(plot[[17]], plot[[9]], plot[[4]],
+            plot[[5]], plot[[6]], plot[[7]],
+            plot[[8]], plot[[10]], plot[[11]], plot[[12]], ncol = 3)
 
 
 #map of balgium 
-post_expo <- train %>% group_by(postal) %>% summarize(num = n(), tot_freq = sum(freq))
+post_expo <- train %>% group_by(postal) %>% summarize(tot_freq = sum(claimAm))
 post_expo %>% slice(1:5)
 
 belgium_shape_sf <- left_join(belgium_shape_sf, post_expo, by = c("POSTCODE" = "postal"))
@@ -56,14 +67,15 @@ labs(fill = "Relative\nclaimamount") +
 scale_fill_brewer(palette = "Blues", na.value = "white") +
 theme_bw()
 
+#basic plots for variables
 
 train %>% plot_bar()
 train %>% plot_histogram()
 
 
 Data_group <- splitmix(train)
-Data_quanti <- Data_nozero[Data_group$col.quant]
-Data_quali <- Data_nozero[Data_group$col.qual]
+Data_quanti <- train[Data_group$col.quant]
+Data_quali <- train[Data_group$col.qual]
 
 tree <- hclustvar(X.quanti = Data_quanti, X.quali = Data_quali)
 plot(tree)
--- Severity_Analysis/modelbuilding.r
+++ Severity_Analysis/modelbuilding.r
@@ -2,22 +2,17 @@
 #student numbers: r0811036
 library(ggplot2)
 library(dplyr)
-library(visdat)
 library(httpgd)
-library(skimr)
-library(DataExplorer)
-library(ClustOfVar)
-library(PCAmixdata)
 library(sf)
 library(moments)
 library(mgcv)
 library(caTools)
 library(caret)
-library(kable)
 library(tmap)
 library(rbin)
 library(data.table)
 library(glmnet)
+library(readxl)
 hgd()
 hgd_browse()
 KULbg <- "#116e8a"
@@ -28,11 +23,21 @@ KULbg <- "#116e8a"
 toledo <- read.csv("toledo files//Assignment.csv", ",", header = T)
 Mdata <- read.csv("data.csv", ",", header = T)
 belgium_shape_sf <- st_read('Severity_Analysis//shape file Belgie postcodes//npc96_region_Project1.shp', quiet = TRUE)
-
 belgium_shape_sf <- st_transform(belgium_shape_sf, "+proj=longlat +datum=WGS84")
 class(belgium_shape_sf)
+#name change
 Mdata$claimAm <- toledo$chargtot
+#ann freq delete
 data <- Mdata %>% select(-freq_ann)
+
+
+# adding long an lat from special dataframe post_dt (can take 5 min to load )
+
+
+for( i in 1:length(data$postal)){
+  data$long[i] <- post_dt$long[data$postal[i] == post_dt$POSTCODELA]
+  data$lat[i] <- post_dt$lat[data$postal[i] == post_dt$POSTCODELA]
+}
 
 # factor as rdataset
 data <- as.data.frame(data)
@@ -44,19 +49,22 @@ Data_nozero <- Data %>% select(-X, - expo, - lnexpo)
 Data_nozero <- subset(Data_nozero, Data$claimAm != 0)
 dim(Data_nozero)
 
-Data_nozero$claimAm <- Data_nozero$claimAm
+# do we need log or not??? 
+Data_nozero$logclaimAm <- log(Data_nozero$claimAm)
 
-#binning postalcode and age 
+# negative claim amount??? 
+Data_nozero$claimAm[Data_nozero$claimAm < 1]
+Data_nozero$logclaimAm[Data_nozero$claimAm < 1]
 
-
+#binning age 
 bins <- rbin_quantiles(Data_nozero, claimAm, ageph, 10)$bins
 bins$cut_point
 breaks <- c(0, 25, 29, 33, 37, 42, 46, 50, 56, 65, 100)
 group_ageph <- cut(Data_nozero$ageph,
                     breaks = breaks, include.lowest = T,
-                    right = FALSE, labels = bins$cut_point)
+                    right = FALSE)
 
-Data_nozero$ageph <- group_ageph
+Data_nozero$group_ageph <- group_ageph
 
 #split data in test and train data with stratified sampling 
 # the dependend variable is factorized to do the split 
@@ -68,43 +76,93 @@ test <- Data_nozero[-trainIndex,]
 
 class(train)
 
-write.csv(train, file = "Severity_Analysis/train.csv" )
+write.csv(train, file = "Severity_Analysis/train.csv")
+
+levels(train$group_ageph)
+levels(Data_nozero$group_ageph)
+#####linear model  ####
+lm <- lm(claimAm ~ agecar + sexph + fuel + split + use + fleet + sportc + cover + power, data = train)
+summary(lm)
+
+
+### test glm and gam with Age 
+a <- seq(min(train$ageph), max(train$ageph))
+
+#glm age not binned 
+glm_age <- glm(claimAm ~ ageph, data = train, family = Gamma(link = "log"))
+pred_glm_age <- predict(glm_age, newdata = data.frame(ageph = a),
+                          type = "terms", se.fit = TRUE)
+b_glm_age <- pred_glm_age$fit
+l_glm_age <- pred_glm_age$fit - qnorm(0.975) * pred_glm_age$se.fit
+u_glm_age <- pred_glm_age$fit + qnorm(0.975) * pred_glm_age$se.fit
+df <- data.frame(a, b_glm_age, l_glm_age, u_glm_age)
+
+ggplot(df) +
+geom_line(aes(x = a, y = b_glm_age)) +
+geom_line(aes(x = a, y = l_glm_age), linetype = 3) +
+geom_line(aes(x = a, y = u_glm_age), linetype = 3)
+
+#glm age binned 
+# note the cut is done on A  as it needs the same factorization as age 
+glm_grouped_age <- glm(claimAm ~ group_ageph, data = train, family = Gamma(link = "log"))
+
+pred_glm_grouped_age <- predict(glm_grouped_age, newdata = data.frame(group_ageph = cut(a,
+                    breaks = breaks, include.lowest = T,
+                    right = FALSE)),
+                          type = "terms", se.fit = TRUE)
+b_glm_grouped_age <- pred_glm_grouped_age$fit
+l_glm_grouped_age <- pred_glm_grouped_age$fit - qnorm(0.975) * pred_glm_grouped_age$se.fit
+u_glm_grouped_age <- pred_glm_grouped_age$fit + qnorm(0.975) * pred_glm_grouped_age$se.fit
+df <- data.frame(a, b_glm_grouped_age, l_glm_grouped_age, u_glm_grouped_age)
+
+ggplot(df) +
+geom_line(aes(x = a, y = b_glm_grouped_age)) +
+geom_line(aes(x = a, y = l_glm_grouped_age), linetype = 3) +
+geom_line(aes(x = a, y = u_glm_grouped_age), linetype = 3)
+
+#gam age 
+gam_age <- gam(claimAm ~ s(ageph), data = train, family = Gamma(link = "log"))
+plot(gam_age, scheme = 1)
+
+
+# estimate expected claim amount with spatial effects 
+post_dt <- st_centroid(belgium_shape_sf)
+post_dt$long <- do.call(rbind, post_dt$geometry)[, 1]
+post_dt$lat <- do.call(rbind, post_dt$geometry)[, 2]
+gam_spatial <- gam(claimAm ~s(long, lat, bs = "tp"), family = Gamma(link="log"), data = train)
+plot(gam_spatial, scheme = 2)
+
+pred  <- predict(gam_spatial, newdata = post_dt, type= "terms", terms = 's(long,lat)')
+pred_dt <- data.frame(pc = post_dt$POSTCODELA, 
+                      long = post_dt$long, 
+                      lat = post_dt$lat, pred)
+names(pred_dt)[4] <- "fit_spatial"
+
+
+belgium_shape_sf <- left_join(belgium_shape_sf, pred_dt, by = c("POSTCODELA" = "pc"))
+
+ggplot(belgium_shape_sf)+ 
+geom_sf(aes(fill = fit_spatial), colour = NA) + 
+ggtitle("claim amount")+
+scale_fill_gradient(low = "#99CCFF", 
+                    high = "#003366")+
+                    theme_bw()
+
+#binning spatial effect 
 
 
 
-# GLM model to fit the data using gamma distribution
 
-#linear model 
-lm <- lm(claimAm ~ ., data = train)
-
-myGrid <- expand.grid(
-  alpha=0:1,
-  lambda = seq(0.0001, 0.5, length = 10)
-)
-
-myControl <- trainControl(
-  method = "cv", 
-  number = 5, 
-  verboseIter = TRUE
-)
-
-glm1 <- train(claimAm~ cover + power+sportc+fleet + use + split +  sexph + agecar + ageph,
-              data = train, family = Gamma(link = "log"),
-            method = "glmnet", 
-            tuneGrid = myGrid, 
-            trControl = myControl)
-plot(glm1$finalModel)
-
-pred_glm1 <- predict(glm1)
-gam1 <- train(claimAm~ s(split, sp=1.2, k=5, bs="cr"), 
-              data = train, faimy= Gamma(link = "log"), 
-              method = "gam")
-
-gam1 <- gam(claimAm ~ s(ageph, bs = "cr"), data = train, family = Gamma(link = "log"), method = "REML")
-print(gam1)
-gam1$sp 
+#setting op the dataframe to predict it with spatial effects 
+post_dt$fuel <- train$fuel[1]
+post_dt$use <- train$use[1]
+post_dt$cover <- train$cover[1]
+post_dt$sportc <- train$sportc[1]
+post_dt$agecar <- train$agecar[1]
+post_dt$sexph <- train$sexph[1]
+post_dt$split <- train$split[1]
+post_dt$power <- train$power[1]
+post_dt$group_ageph <- train$group_ageph [1]
 
 
-plot(gam1, pages =1 , scheme=1)
-a <- min(train$ageph):max(train$ageph)
-
+### using lasso and ridge regresion with carret 
