8c5d499d60b9e98c825ffbd7380c81202d77d41d
korilium <ignace.decocq@hotmail.com>
2021-04-27 18:57:22 +0200

    remove Rdata and reordering code

--- Severity_Analysis/modelbuilding.r
+++ Severity_Analysis/modelbuilding.r
@@ -61,7 +61,31 @@ test <- Data[-trainIndex,]
 #logclaims
 train$logclaimAm <- log(train$claimAm)
 
-#binning age
+#################binning age and spatial  ####################################
+
+#extracting  Long and Lat
+post_dt <- st_centroid(belgium_shape_sf)
+post_dt$LONG<- do.call(rbind, post_dt$geometry)[,1]
+post_dt$LAT <- do.call(rbind, post_dt$geometry)[,2]
+
+
+pred  <- predict(gam_spatial, newdata = post_dt, type= "terms", 
+                 terms = 's(LONG,LAT)')
+pred_dt <- data.frame(pc = post_dt$POSTCODE, 
+                      LONG= post_dt$LONG, 
+                      LAT = post_dt$LAT, pred)
+names(pred_dt)[4] <- "fit_spatial"
+
+# creating dataframe with binned spatial effect 
+names(pred_dt)[4] <- "fit_spatial"
+train_geo <- train
+train_geo <- left_join(train_geo, pred_dt, by = c("postal" = "pc"))
+train_geo$geo <- as.factor(cut(train_geo$fit_spatial, 
+     breaks = classint_fisher$brks, right=FALSE, 
+     include.lowest=TRUE, dig.lab=2))
+
+freq_gam_geo <- gam(claimAm ~ cover + fuel + s(ageph) + geo, data = train_geo, family = Gamma(link = "log") )
+
 #binning age 
 
 #getting the dataset for age 
@@ -122,56 +146,6 @@ train_nozero <- subset(train, train$claimAm != 0)
 dim(train_nozero)
 
 
-levels(train$group_ageph)
-levels(train_nozero$group_ageph)
-#####linear model  ####
-lm <- lm(claimAm ~ agecar + sexph + fuel + split + 
-          use + fleet + sportc + cover + power, data = train)
-summary(lm)
-
-
-### test glm and gam with Age 
-a <- seq(min(train_nozero$ageph), max(train_nozero$ageph))
-
-#glm age not binned 
-glm_age <- glm(claimAm ~ ageph,
-               data = train_nozero, family = Gamma(link = "log"))
-
-pred_glm_age <- predict(glm_age, newdata = data.frame(ageph = a),
-                          type = "terms", se.fit = TRUE)
-b_glm_age <- pred_glm_age$fit
-l_glm_age <- pred_glm_age$fit - qnorm(0.975) * pred_glm_age$se.fit
-u_glm_age <- pred_glm_age$fit + qnorm(0.975) * pred_glm_age$se.fit
-df <- data.frame(a, b_glm_age, l_glm_age, u_glm_age)
-
-ggplot(df) +
-geom_line(aes(x = a, y = b_glm_age)) +
-geom_line(aes(x = a, y = l_glm_age), linetype = 3) +
-geom_line(aes(x = a, y = u_glm_age), linetype = 3)
-
-#glm age binned
-# note the cut is done on A  as it needs the same factorization as age 
-glm_grouped_age <- glm(claimAm ~ group_ageph,
-                       data = train_nozero, family = Gamma(link = "log"))
-
-pred_glm_grouped_age <- predict(glm_grouped_age,
-                    newdata = data.frame(group_ageph = 
-                    cut(a, breaks = breaks, include.lowest = T,
-                    right = FALSE)), type = "terms", se.fit = TRUE)
-
-b_glm_grouped_age <- pred_glm_grouped_age$fit
-l_glm_grouped_age <- pred_glm_grouped_age$fit - qnorm(0.975) * pred_glm_grouped_age$se.fit
-u_glm_grouped_age <- pred_glm_grouped_age$fit + qnorm(0.975) * pred_glm_grouped_age$se.fit
-df <- data.frame(a, b_glm_grouped_age, l_glm_grouped_age, u_glm_grouped_age)
-
-ggplot(df) +
-geom_line(aes(x = a, y = b_glm_grouped_age)) +
-geom_line(aes(x = a, y = l_glm_grouped_age), linetype = 3) +
-geom_line(aes(x = a, y = u_glm_grouped_age), linetype = 3)
-
-#gam age 
-gam_age <- gam(claimAm ~ s(ageph), data = train_nozero, family = Gamma(link = "log"))
-plot(gam_age, scheme = 1)
 
 
 # estimate expected claim amount with spatial effects 
@@ -283,6 +257,8 @@ splits_evtree = function(evtreemodel, GAMvar, DTvar){
 claimAm_splits_ageph <- splits_evtree(evtree_claimAm_ageph, 
                                       gam_ageph$ageph, 
                                       train_nozero$ageph)
-claimAm_splits_ageph
+claimAm_splits_ageph     
 
 ### using lasso and ridge regresion with carret 
+
+
