6447e0fd0ebef68a3e790361904311a5a2a389da
Mathijs Gerits <MathijsGerits@MacBook-Air-van-Mathijs.local>
2021-04-30 19:23:28 +0200

    30/04

--- Frequency_Analysis/Freq_GBM.R
+++ Frequency_Analysis/Freq_GBM.R
@@ -26,7 +26,7 @@ gbm_0 <- gbm(freq ~ offset(log(expo)) + ageph + geo + agecar + sexph + fuel # mi
              + split + use + fleet + sportc + cover + power,
              data = gbm_train,
              distribution = "poisson",
-             n.trees = 500,
+             n.trees = 1000,          # First try 500 -> insufficient, then 1000 -> insufficient for cv, then 1500
              interaction.depth = 2,   # model up to 2-way interactions
              shrinkage = 0.01,        # 0.01 learning rate (low -> better performance, increase computation time)
              bag.fraction = 0.75,     # each step uses a subsample size delta*n
@@ -38,20 +38,58 @@ gbm_0 <- gbm(freq ~ offset(log(expo)) + ageph + geo + agecar + sexph + fuel # mi
              n.minobsinnode = 10000)  # minimum number of observations in the terminal nodes of the trees
 
 # Tuning parameters: n.trees (T) and interaction.depth (d)
-# Hyperparameters: shrinkage (lambda = 0.01), bag.fraction (delta = 0.75) -> Values based on paper of Roel
+# Hyper Parameters: shrinkage (lambda = 0.01), bag.fraction (delta = 0.75) -> Values based on paper of Roel
+
+gbm_0   # There were 11 predictors of which 8 had non-zero influence.
+summary(gbm_0) # use, fleet, sportc do not have a non-zero influence 
+    # ageph, split and geo are the most important ! 
+
+plot(gbm_0$train.error, type = "l", main = "Training error per iteration",
+     xlab = "Number of trees")
+
+### Looking for best hyperparameters (max n.tree and cv.folds)
+
+gbm_0$train.error[gbm_0$n.trees]
+  # CV = 5
+  # n.tree = 500  -> 0.7515610
+  # n.tree = 1000 -> 0.7505098
+  # n.tree = 1500 -> 0.7502090
+
+  # CV = 10
+  # n.tree = 500  -> 0.7515611
+  # n.tree = 1000 -> 0.7504996
+  # n.tree = 1500 -> 0.7501954
+
+pretty.gbm.tree(gbm_0, i.tree = gbm_0$n.trees) # Gives the fitted tree of the last iteration
 
 
+### Find optimal n.trees 
+gbm_best_i_oob <- gbm.perf(gbm_0, method = "OOB", oobag.curve = TRUE, overlay = TRUE)
+gbm_best_i_oob 
+  # CV = 5
+  # n.tree = 500  -> 500
+  # n.tree = 1000 -> 681 (!)
+  # n.tree = 1500 -> 692
+  
+  # CV = 10
+  # n.tree = 500  -> 500
+  # n.tree = 1000 -> 658
+  # n.tree = 1500 -> 672
+
+gbm_best_i_cv <- gbm.perf(gbm_0, method = "cv")
+gbm_best_i_cv 
+  # CV = 5
+  # n.tree = 500  -> 500
+  # n.tree = 1000 -> 1000
+  # n.tree = 1500 -> 1479
+  
+  # CV = 10
+  # n.tree = 500  -> 500
+  # n.tree = 1000 -> 1000
+  # n.tree = 1500 -> 1500
 
 
-
-
-
-
-
-
-
-
-
+# use max n.trees = 1000, with optimal n.trees = 681 and 5fold cv
 
 
 
